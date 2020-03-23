25395bbdedbd0a00fbac1e8697dd10928d7c4bfd
Cas van der Laan <cas.van.der.laan@cern.ch>
2020-03-23 15:31:24 +0100

    Fix tests

--- hepscore/hepscore.py
+++ hepscore/hepscore.py
@@ -266,7 +266,8 @@ class HEPscore(object):
             runDir = self.resultsdir + "/" + benchmark[:-4] + "/run" + str(i)
             logsFile = runDir + "/" + self.cec + "_logs"
 
-            os.makedirs(runDir)
+            if self.confobj['replay'] == False:
+                os.makedirs(runDir)
 
             commands = {'docker': "docker run --rm --network=host -v " +
                         runDir + ":/results ",
