b3fdde4d2737552acd901e3db32b52b2f7bfa3bf
Cas van der Laan <cas.van.der.laan@cern.ch>
2020-04-30 14:26:29 +0200

    Add copyright lines to source files

--- hepscore/__init__.py
+++ hepscore/__init__.py
@@ -1,3 +1,7 @@
 # __init_.py
 
+# Copyright 2019-2020 CERN. See the COPYRIGHT file at the top-level directory
+# of this distribution. For licensing information, see the COPYING file at
+# the top-level directory of this distribution.
+
 from .hepscore import HEPscore
